ddc1d51a34cb9e42e6bc56bd629dd4adfb476afc
sarikasinghal <80735730+sarikasinghal@users.noreply.github.com>
2021-06-20 22:26:54 -0500

    adding week 2 assingments

---
+++ Week2/assignment_00_SinghalSarika.R
@@ -0,0 +1,59 @@
+# Assignment: ASSIGNMENT 0
+# Name: Singhal, Sarika
+# Date: 2021-06-14
+
+# Basics
+
+## Add 8 and 5
+x<-8+5
+print(x)
+
+## Subtract 6 from 22
+y<-22-6
+print(y)
+
+## Multiply 6 by 7
+z<-6*7
+print(z)
+
+## Add 4 to 6 and divide the result by 2
+a<-(4+6)/2
+print(a)
+
+## Compute 5 modulo 2
+b<-5%%2
+print(b)
+
+## Assign the value 82 to the variable x
+## Print x
+x <- 82
+print(x)
+
+## Assign the value 41 to the variable y
+## Print y
+y <- 41
+print(y)
+
+## Assign the output of x + y to the variable z
+## Print z
+z <- x + y
+print(z)
+
+## Assign the string value "DSC520" to the variable class_name
+## Print the value of class_name
+class_name <- 'DSC520'
+print(class_name)
+
+## Assign the string value of TRUE to the variable is_good
+## Print the value of is_good
+is_good <- TRUE
+print(is_good)
+
+## Check the class of the variable is_good using the `class()` function
+class(is_good)
+
+## Check the class of the variable z using the `class()` function
+class(z)
+
+## Check the class of the variable class_name using the class() function
+class(class_name)
