3f8d004a00a5a9c7f39a55597dac9e65d9fda51e
sarikasinghal <80735730+sarikasinghal@users.noreply.github.com>
2021-07-24 18:55:03 -0500

    adding week 7 assingment

--- week7/assignment_05_SinghalSarika02.R
+++ week7/assignment_05_SinghalSarika02.R
@@ -10,12 +10,6 @@ setwd("~/Documents/repo/Week2/dsc520/data")
 studentsurvey <- read.csv("student-survey.csv")
 str(studentsurvey)
 
-## TimeReading vs. TimeTV
-cor(studentsurvey$TimeReading, studentsurvey$TimeTV/60, method = c("pearson", "kendall", "spearman"))
-### TimeReading vs. Happiness
-cor(studentsurvey$TimeReading, studentsurvey$Happiness, method = c("pearson", "kendall", "spearman"))
-### TimeReading vs. Gender
-cor(studentsurvey$TimeReading, studentsurvey$Gender, method = c("pearson", "kendall", "spearman"))
 
 #2. Examine the Survey data variables. What measurement is being used for the variables? Explain what effect changing the measurement being used for the variables would have on the covariance calculation. Would this be a problem? Explain and provide a better alternative if needed.
 
@@ -47,4 +41,60 @@ cor(studentsurvey)
 
 #Correlation method can be pearson, spearman or kendall
 
-#TimeReading : 
+#The pearson correlation coefficient measure the linear dependence between two variables.
+
+#If method is “kendall” or “spearman”, Kendall’s tau or Spearman’s rho statistic is used to estimate a rank-based measure of association. These are more robust and have been recommended if the data do not come from a bivariate normal distribution.
+
+pearson_test<-cor.test(studentsurvey$TimeReading, studentsurvey$TimeTV/60, method="pearson")
+pearson_test
+
+#spearman_test<-cor.test(studentsurvey$TimeReading, studentsurvey$TimeTV/60, method="spearman")
+
+spearman_test<-cor.test(studentsurvey$TimeReading, studentsurvey$TimeTV/60, method="spearman",exact=FALSE)
+
+spearman_test
+
+plot(studentsurvey$TimeReading, studentsurvey$TimeTV/60)
+
+#Looking at the plot Pearson_test method sounds the best for this
+
+#4. Perform a correlation analysis of:
+
+#All Variables
+
+studentsurvey <- read.csv("student-survey.csv")
+cor(studentsurvey)
+
+## TimeReading vs. TimeTV
+cor(studentsurvey$TimeReading, studentsurvey$TimeTV/60, method = c("pearson", "kendall", "spearman"))
+### TimeReading vs. Happiness
+cor(studentsurvey$TimeReading, studentsurvey$Happiness, method = c("pearson", "kendall", "spearman"))
+### TimeReading vs. Gender
+cor(studentsurvey$TimeReading, studentsurvey$Gender, method = c("pearson", "kendall", "spearman"))
+
+#Repeat your correlation test in step 2 but set the confidence interval at 99%
+timereadingvstimetv<-cor.test(studentsurvey$TimeReading, studentsurvey$TimeTV/60,method = c("pearson", "kendall", "spearman"),exact = NULL, conf.level = 0.95, continuity = FALSE)
+
+#Describe what the calculations in the correlation matrix suggest about the relationship between the variables. Be specific with your explanation.
+names(timereadingvstimetv)
+timereadingvstimetv$conf.int
+timereadingvstimetv$p.value
+timereadingvstimetv$method
+timereadingvstimetv$null.value
+timereadingvstimetv$statistic
+
+#Most variables show very little relationship and negative correlationship
+
+#Calculate the correlation coefficient and the coefficient of determination, describe what you conclude about the results.
+cofofdet <- lm( studentsurvey$Gender ~ studentsurvey$TimeReading + TimeTV + Happiness, data=studentsurvey)
+summary(cofofdet)
+
+#Based on your analysis can you say that watching more TV caused students to read less? Explain.
+
+#Yes, That what it looks like
+
+#Pick three variables and perform a partial correlation, documenting which variable you are “controlling”. Explain how this changes your interpretation and explanation of the results.
+install.packages('ppcor')
+install.packages('MASS')
+library(ppcor)
+pcor(studentsurvey)
